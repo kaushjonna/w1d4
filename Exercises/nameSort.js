0b7e5c6590bc3bd50bf51b112bd6114fb67d121f
kaushjonna <kaush.jonna@gmail.com>
2019-02-07 12:44:09 -0500

    amended the function to be simpler for age sorting scenario

--- Exercises/nameSort.js
+++ Exercises/nameSort.js
@@ -1,7 +1,7 @@
 var students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
-  { id: 3, name: "alex",     age: 42 },
+  { id: 3, name: "alex",     age: 22 },
   { id: 4, name: "alex",     age: 30 }
 ];
 
@@ -15,16 +15,7 @@ students.sort(function(a,b){
   if (nameA > nameB) {
     return 1;
   }
-
-  if(a.age > b.age){
-    return 1;
-  }
-
-  if(a.age < b.age){
-    return -1;
-  }
-  
-  return 0;
+  return a.age-b.age;
 
 });
 
